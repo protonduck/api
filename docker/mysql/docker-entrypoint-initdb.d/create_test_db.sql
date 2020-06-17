54f679fea8d58e2c53f46e996c7405f444067f2e
Ivan Orlov <ivan@orlov.io>
2020-06-17 21:21:10 +0600

    Docker

---
+++ docker/mysql/docker-entrypoint-initdb.d/create_test_db.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE IF NOT EXISTS `bookmarks_test`;
+GRANT ALL ON `bookmarks_test`.* TO 'user'@'%';
--- environments/dev/common/config/test-local.php
+++ environments/dev/common/config/test-local.php
@@ -2,7 +2,7 @@
 return [
     'components' => [
         'db' => [
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced_test',
+            'dsn' => 'mysql:host=db;dbname=bookmarks_test',
         ],
     ],
 ];
